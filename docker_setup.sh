620a316a00de1d8f145d932ce00d3dd0a0288715
root <root@ip-172-31-34-184.eu-west-1.compute.internal>
2023-10-17 10:57:37 +0000

    Docker Files

---
+++ docker_setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+apt --help >>/dev/null
+if [ $? -eq 0 ]
+   then 
+echo " INSTALLING DOCKER IN UBUNTU"
+echo
+sudo apt update
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y  
+sudo docker run hello-world
+    else
+echo " INSTALLING DOCKER IN CENTOS"
+echo
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y   
+sudo systemctl start docker
+sudo docker run hello-world
+fi
+echo " Installing Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
